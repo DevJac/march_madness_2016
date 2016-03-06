c273b3d752423f2558ac7531a9ad196983c48cdc
Devin Jacobs <dbuttonz@gmail.com>
2016-03-06 14:37:25 -0700

    scrape.py gets school name map

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from urlparse import urljoin
 from time import sleep
 import re
+from tqdm import tqdm
 from retrying import retry
 import lxml.html
 from selenium import webdriver
@@ -13,14 +14,17 @@ ALL_SCHOOLS_PAGE = 'http://www.sports-reference.com/cbb/schools/'
 gamelog_links = set()
 
 
+school_name_map = {}
+
+
 html = lxml.html.parse(ALL_SCHOOLS_PAGE)
 # This will find all links on the page that refer to schools.
 # The page has one link to itself as "/cbb/schools/" which we ignore.
 school_relative_links = (
-    set(html.xpath("//a[contains(@href, '/cbb/schools/')]/@href"))
+    set(html.xpath("//td[text() = '2016']/parent::tr//a/@href"))
     .difference(['/cbb/schools/']))
 school_absolute_links = [urljoin(ALL_SCHOOLS_PAGE, url)
-                         for url in school_relative_links]
+                         for url in school_relative_links][:10]
 
 gamelog_links = []
 for url in school_absolute_links:
@@ -35,6 +39,12 @@ def scrape_gamelog(driver, gamelog_link):
     year = match.group(2)
     driver.get(gamelog_link)
     sleep(2)
+    html = lxml.html.fromstring(driver.page_source)
+    for school_link in html.xpath(
+            "//table[@id='stats_basic']//a[contains(@href, '/cbb/schools/')]"):
+        school_name_map[school_link.text.strip()] = (
+            re.search('/cbb/schools/([a-z-]+)/', school_link.attrib['href'])
+            .group(1))
     (driver
         .find_element_by_xpath("//span[contains(@onclick, 'table2csv')]")
         .click())
@@ -42,7 +52,6 @@ def scrape_gamelog(driver, gamelog_link):
     csv = driver.find_element_by_xpath('//pre').text
     with open('data/{}{}.csv'.format(team, year), 'w') as f:
         f.write(csv)
-    print team, year
 
 
 # The gamelog page has a button that will turn the stats into CSV.
@@ -51,10 +60,11 @@ driver_profile = webdriver.FirefoxProfile()
 driver_profile.set_preference('webdriver.load.strategy', 'unstable')
 driver = webdriver.Firefox(driver_profile)
 driver.implicitly_wait(10)
-for i, gamelog_link in enumerate(gamelog_links, 1):
+for gamelog_link in tqdm(gamelog_links):
     try:
-        print '{:d}/{:d} {}'.format(i, len(gamelog_links), gamelog_link)
         scrape_gamelog(driver, gamelog_link)
     except NoSuchElementException:
         pass
 driver.close()
+with open('school_name_map.txt', 'w') as f:
+    f.writelines('{} {}\n'.format(v, k) for k, v in school_name_map.iteritems())
