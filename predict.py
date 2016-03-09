85a62eff5c0a84ec0adc230d89605b52e2536ff9
Devin Jacobs <dbuttonz@gmail.com>
2016-03-08 19:42:43 -0700

    Award teams for +5 points

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import date
 import networkx as nx
 from database import Game, new_session
 
@@ -14,3 +15,15 @@ def team_strength(winner_losers):
         loss_graph.add_edge(winner, loser, weight=weight)
     loss_ranks = nx.pagerank(loss_graph)
     return {k: v - loss_ranks[k] for k, v in nx.pagerank(win_graph).iteritems()}
+
+
+session = new_session()
+r = session.query(Game).filter(Game.result == 'win',
+                               Game.date > date(2015, 6, 1))
+wl = [(g.team, g.opponent, 3 + min(5, g.points - g.opp.points))
+      for g in r]
+ts = team_strength(wl)
+for team, strength in sorted(ts.iteritems(),
+                             key=lambda a: a[1],
+                             reverse=True)[:100]:
+    print '{} {:.0f}'.format(team, strength*10000)
